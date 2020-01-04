94b47a0f7685fbf4e22bd3481a73c8fbe222ef78
peipei0w0 <59371107+peipei0w0@users.noreply.github.com>
2020-01-04 13:11:44 +0800

    Add files via upload

---
+++ Division.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num1,num2;
+	cout<<"請輸入要相除的2個數字"<<endl;
+	cin>>num1>>num2; 
+	cout<<"The answer is : "<<num1/num2<<"..."<<num1%num2<<endl;
+} 
---
+++ Substraction.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num1,num2;
+	cout<<"請輸入要相減的2個數字"<<endl;
+	cin>>num1>>num2; 
+	cout<<"兩數之差為:"<<num1-num2<<endl;
+} 
